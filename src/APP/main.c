59855a6c8d553ab7538450e13e0c07f6867101ef
Yahia-Ghadiry <yahiaghadiry@gmail.com>
2024-09-04 02:33:25 +0300

    Finnished Application 2 for Task 3

--- src/APP/main.c
+++ src/APP/main.c
@@ -21,6 +21,12 @@
 #define KEYPAD_SYMBOlS (volatile char[4 * 4]){'7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', ON_OFF_KEY, '0', '=', '+'} 
 #define ON_OFF_KEY_INDEX 12
 
+#define VOTEING_A_INDEX 3
+#define VOTEING_B_INDEX 7
+#define VOTEING_C_INDEX 11
+#define VOTEING_D_INDEX 15
+
+
 void Mode1(u8 u8Mode_ID);
 void Mode2(u8 u8Mode_ID);
 
@@ -44,9 +50,7 @@ int main(void)
         }
         else if (u8read_switch == BUTTON_DOWN)
         {
-            //tmp
-            Mode1(MODE_2);
-            
+            Mode2(MODE_2);
         }
     }
 }
@@ -87,43 +91,70 @@ void Mode1(u8 u8Mode_ID)
 
 void Mode2(u8 u8Mode_ID)
 {
-    u8 u8Counter = 0;
-    
-    u8 u8read_button1_prev = BUTTON_UP;
-    u8 u8read_button2_prev = BUTTON_UP;
-    u8 u8read_button1 = BUTTON_UP;
-    u8 u8read_button2 = BUTTON_UP;
-
     u8 u8read_switch = u8Mode_ID;
-    
-    LCD_vClearScreen();
-    LCD_vSendInt(u8Counter);
+    u8 u8KeyPad_pressed = KEYPAD_NO_PRESSED_ALL;
+    u8 u8KeyPad_pressed_prev = KEYPAD_NO_PRESSED_ALL;
+        
+    u8 u8VoterA = 0;
+    u8 u8VoterB = 0;
+    u8 u8VoterC = 0;
+    u8 u8VoterD = 0;
 
+    LCD_vClearScreen();
+    
     while(INFINTE)
     {
         u8read_switch = BUTTON_u8ReadButton(SWITCH_PORT_ID, SWITCH_PIN_ID, BUTTON_PULLED_UP);
         if (u8read_switch != u8Mode_ID)
             break;
-
-        
-        if(u8read_button1 == BUTTON_DOWN && u8read_button1 != u8read_button1_prev)
+    
+        u8KeyPad_pressed = KEYPAD_u8KeyPressed(KEYPAD_PULL_UP);
+            
+        if (u8KeyPad_pressed != KEYPAD_NO_PRESSED_ALL && u8KeyPad_pressed != u8KeyPad_pressed_prev)
         {
+            if (u8KeyPad_pressed == ON_OFF_KEY_INDEX)
+            {
+                u8VoterA = 0;
+                u8VoterB = 0;
+                u8VoterC = 0;
+                u8VoterD = 0;
+            }
+            else
+            {
+                switch (u8KeyPad_pressed)
+                {
+                    case VOTEING_A_INDEX:
+                        u8VoterA++;
+                        break;
+                    case VOTEING_B_INDEX:
+                        u8VoterB++;
+                        break;
+                    case VOTEING_C_INDEX:
+                        u8VoterC++;
+                        break;
+                    case VOTEING_D_INDEX:
+                        u8VoterD++;
+                        break;
+                }
+            }
+            
             LCD_vClearScreen();
-            u8Counter++;
-            LCD_vSendInt(u8Counter);
+            LCD_vSendString("A=");
+            LCD_vSendInt(u8VoterA);
+            LCD_vMoveCursor(LCD_LINE1_POSITION | LCD_CHAR9_POSITION);
+            LCD_vSendString("B=");
+            LCD_vSendInt(u8VoterB);
+            LCD_vMoveCursor(LCD_LINE2_POSITION | LCD_CHAR1_POSITION);
+            LCD_vSendString("C=");
+            LCD_vSendInt(u8VoterC);
+            LCD_vMoveCursor(LCD_LINE2_POSITION | LCD_CHAR9_POSITION);
+            LCD_vSendString("D=");
+            LCD_vSendInt(u8VoterD);
         }
 
-
-        if(u8read_button2 ==  BUTTON_DOWN && u8read_button2 != u8read_button2_prev)
-        {
-            LCD_vClearScreen();
-            u8Counter = 0;
-            LCD_vSendInt(u8Counter);
-        }
-
-        u8read_button1_prev = u8read_button1;
-        u8read_button2_prev = u8read_button2;
+        u8KeyPad_pressed_prev = u8KeyPad_pressed;
 
         _delay_ms(REACTION_DELAY);
+
     }
 }
