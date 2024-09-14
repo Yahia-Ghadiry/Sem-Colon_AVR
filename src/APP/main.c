84d42f0b42f1c5feaf14f6884f19248dfca5ccca
Yahia-Ghadiry <yahiaghadiry@gmail.com>
2024-09-14 19:29:23 +0300

    Added part 2 in task 5

--- src/APP/main.c
+++ src/APP/main.c
@@ -1,3 +1,4 @@
+#include "HAL/BUTTON/Button.h"
 #define F_CPU 8000000UL
 
 #include "LIB/BitMath.h"
@@ -18,6 +19,11 @@
 #define YELLOW_LED_PIN_ID PIN1_ID
 #define RED_LED_PIN_ID PIN2_ID
 
+#define SWITCH_PORT_ID PORTC_ID
+#define SWITCH_PIN_ID PIN0_ID
+
+#define MODE1_ID BUTTON_UP
+#define MODE2_ID BUTTON_DOWN
 
 extern u32 Timer0_NumOfOV;
 extern u32 Timer0_NumOfCM;
@@ -26,8 +32,13 @@ void vDisplayRemaing();
 
 u32 counterCM = 0;
 
+u8 SecondsPassed = 0;
+
 u8 RemaingSec = 10;
 u8 LightMode = 0;
+
+void vMode1(u8 u8ModeID);
+void vMode2(u8 u8ModeID);
 
 int main(void)
 {
@@ -37,7 +48,8 @@ int main(void)
     LED_vInit(LEDS_PORT_ID, YELLOW_LED_PIN_ID, LED_MODE_1_ON);
     LED_vInit(LEDS_PORT_ID, RED_LED_PIN_ID, LED_MODE_1_ON);
 
-    
+    BUTTON_vInitButton(SWITCH_PORT_ID, SWITCH_PIN_ID, BUTTON_PULLED_UP);
+
     LCD_vInit(LCD_FUNCTION_SET_BAISE | LCD_2_LINE | LCD_5x8, LCD_DISPLAY_ON_OFF_CONTROL_BAISE |  LCD_DISPLAY_ON | LCD_CURSOR_OFF | LCD_BLINK_OFF, LCD_ENTRY_MODE_SET_BAISE | LCD_CURSOR_INC_MODE | LCD_SHIFT_DISPLAY_OFF);
     
     Timer0_vInit();
@@ -51,19 +63,76 @@ int main(void)
     
     while(INFINTE)
     {
+        u8 mode = BUTTON_u8ReadButton(SWITCH_PORT_ID, SWITCH_PIN_ID, BUTTON_PULLED_UP);
+        if (mode == MODE1_ID)
+            vMode1(MODE1_ID);
+        else if (mode == MODE2_ID)
+            vMode2(MODE2_ID);
         
-        _delay_ms(REACTION_DELAY);
     }
 }
 
 ISR(TIMER0_COMP_vect)
 {
     counterCM++;
-
+    
     if(counterCM == SEC_MS)
     {
         counterCM = 0;
         RemaingSec--;
+        SecondsPassed++;
+    }
+}
+void vMode2(u8 u8ModeID)
+{
+    counterCM = 0;
+    SecondsPassed = 0;
+    LED_vTurnOff(LEDS_PORT_ID, GREEN_LED_PIN_ID, LED_MODE_1_ON);
+    LED_vTurnOff(LEDS_PORT_ID, YELLOW_LED_PIN_ID, LED_MODE_1_ON);
+    LED_vTurnOff(LEDS_PORT_ID, RED_LED_PIN_ID, LED_MODE_1_ON);
+    
+    while (true)
+    {
+        u8 mode = BUTTON_u8ReadButton(SWITCH_PORT_ID, SWITCH_PIN_ID, BUTTON_PULLED_UP);
+        if (mode != u8ModeID)
+            break;
+        SecondsPassed %= 12;
+
+        if (SecondsPassed % 2 == 0)
+            LED_vTurnOn(LEDS_PORT_ID, GREEN_LED_PIN_ID, LED_MODE_1_ON);
+        else
+            LED_vTurnOff(LEDS_PORT_ID, GREEN_LED_PIN_ID, LED_MODE_1_ON);
+        
+        if ((SecondsPassed / 2) % 2 == 0)
+            LED_vTurnOn(LEDS_PORT_ID, YELLOW_LED_PIN_ID, LED_MODE_1_ON);
+        else
+            LED_vTurnOff(LEDS_PORT_ID, YELLOW_LED_PIN_ID, LED_MODE_1_ON);
+
+        if ((SecondsPassed / 3) % 2 == 0)
+            LED_vTurnOn(LEDS_PORT_ID, RED_LED_PIN_ID, LED_MODE_1_ON);
+        else
+            LED_vTurnOff(LEDS_PORT_ID, RED_LED_PIN_ID, LED_MODE_1_ON);
+        
+        LCD_vClearScreen();
+        LCD_vSendInt(SecondsPassed);
+        _delay_ms(REACTION_DELAY);
+    }
+}
+
+void vMode1(u8 u8ModeID)
+{
+    counterCM = 0;
+    RemaingSec = 10;
+    LightMode = 0;
+    LED_vTurnOn(LEDS_PORT_ID, GREEN_LED_PIN_ID, LED_MODE_1_ON);
+    LED_vTurnOff(LEDS_PORT_ID, YELLOW_LED_PIN_ID, LED_MODE_1_ON);
+    LED_vTurnOff(LEDS_PORT_ID, RED_LED_PIN_ID, LED_MODE_1_ON);
+
+    while (true)
+    {
+        u8 mode = BUTTON_u8ReadButton(SWITCH_PORT_ID, SWITCH_PIN_ID, BUTTON_PULLED_UP);
+        if (mode != u8ModeID)
+            break;
         if (RemaingSec == 0)
         {
             LightMode++;
@@ -110,9 +179,11 @@ ISR(TIMER0_COMP_vect)
                 break;
         }
         vDisplayRemaing();
+        
+        _delay_ms(REACTION_DELAY);
+    
     }
 }
-
 void vDisplayRemaing()
 {
     LCD_vClearScreen();
